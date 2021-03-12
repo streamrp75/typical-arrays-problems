40113124eeace905771bca33fe05a194945db8d1
streamrp75 <79273733+streamrp75@users.noreply.github.com>
2021-03-12 18:03:32 +0300

    feat: add code file

--- src/index.js
+++ src/index.js
@@ -1,12 +1,45 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (array == undefined || array.length == 0){
+    return 0
+  }
+  let i=0
+  let min_value=array[0]
+  for(i=0; i<array.length;i++)
+  {
+    if (array[i]<min_value)
+    {
+      min_value=array[i]
+    }
+  }
+  return min_value;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (array == undefined || array.length == 0){
+    return 0
+  }
+  let i=0
+  let max_value=array[0]
+  for(i=0; i<array.length;i++)
+  {
+    if (array[i]>max_value)
+    {
+      max_value=array[i]
+    }
+  }
+  return max_value;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (array == undefined || array.length == 0){
+    return 0
+  }
+  let i=0
+  let sum=0
+  for(i=0; i<array.length;i++)
+  {
+    sum+=array[i]
+  }
+  return (sum/array.length);
 }
